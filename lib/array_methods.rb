a019415e2ebd2f48adc07fdca5f989320df62f06
spectacular-system-6224 <d.leslie707@gmail.com>
2020-02-13 11:24:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,34 @@
+
+#def find_element_index(array, value_to_find)
+  #array.find_index(value_to_find)
+#end
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |count|
+
+  if array[count] == value_to_find
+  return count
+    end
+    nil
+  end
+end
+ 
+#fun = [ 0, 47, 88,100]
+
+ #p find_element_index(fun, 100)
+
+=begin
+def find_max_value(array)
+  p array.max
+  puts""
 end
 
-def find_max_value(array)
-  # Add your solution here
-end
+find_max_value(silly)
 
 def find_min_value(array)
-  # Add your solution here
+ p array.min
+ puts""
 end
+
+find_min_value(silly)
+=end
